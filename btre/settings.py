7ecf787a3912a5c324bcb4abaa800cea203c76b5
Bonaventure Ogeto <bonaogeto1@gmail.com>
2020-03-28 18:10:35 +0300

    Pulling data and displaying listings

--- btre/settings.py
+++ btre/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -129,7 +130,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
 
-#media folder settings
+# media folder settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
+from .models import Listing
+
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all()  # fetch all listings
 
-def listing(request):
+    context = {
+        'listings': listings
+    }
+
+    return render(request, 'listings/listings.html', context)
+
+
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
+
 
 def search(request):
     return render(request, 'listings/search.html')
