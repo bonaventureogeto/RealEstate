6ce6433442d20be2b9fcc98d66a5946b35722c05
Bonaventure Ogeto <bonaogeto1@gmail.com>
2020-03-29 19:14:01 +0300

    preserving form input

--- listings/views.py
+++ listings/views.py
@@ -69,7 +69,8 @@ def search(request):
         'listings': queryset_list,
         'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
-        'price_choices': price_choices
+        'price_choices': price_choices,
+        'values': request.GET
     }
 
     return render(request, 'listings/search.html', context)
