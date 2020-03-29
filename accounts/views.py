401632387c88e4efabf186f24d58f291f9de4a44
Bonaventure Ogeto <bonaogeto1@gmail.com>
2020-03-30 02:35:48 +0300

    Logout and nav_bar links

--- accounts/views.py
+++ accounts/views.py
@@ -33,7 +33,7 @@ def register(request):
                     # return redirect('index')
                     user.save()
                     messages.success(
-                        request, 'You are now registered. Proceed to login')
+                        request, 'You are now registered. You can proceed to login')
                     return redirect('login')
 
         else:
@@ -45,14 +45,28 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        # Login user
-        return
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+
+        else:
+            messages.error(request, 'Invalid username or password')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
 
 def logout(request):
-    return redirect('index')
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You are now logged out')
+        return redirect('index')
 
 
 def dashboard(request):
