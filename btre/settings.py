cc82eed0d2d270ba7e578459949477697d3a988b
Bonaventure Ogeto <bonaogeto1@gmail.com>
2020-04-01 00:51:12 +0300

    Added staticfiles

--- btre/settings.py
+++ btre/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = 'btre/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
@@ -154,3 +154,4 @@ MESSAGE_TAGS = {
 }
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
