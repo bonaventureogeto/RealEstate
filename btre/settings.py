593b366668e50b9312f1ae78e2887ed920f94a07
Bonaventure Ogeto <bonaogeto1@gmail.com>
2020-04-02 07:25:20 +0300

    Changed settings

--- btre/settings.py
+++ btre/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ('hetrwyeufvgyfgewufybecbwurwirwgfusygf')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['adventurerealestate.herokuapp.app']
 
 print(ALLOWED_HOSTS)
 # Application definition
