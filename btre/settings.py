5ede2f4a4d2623a521947d5c4f7f08b2301bb13d
Bonaventure Ogeto <bonaogeto1@gmail.com>
2020-04-02 08:21:14 +0300

    Changed settings

--- btre/settings.py
+++ btre/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['adventurerealestate.herokuapp.app']
+ALLOWED_HOSTS = ['*']
 
 print(ALLOWED_HOSTS)
 # Application definition
